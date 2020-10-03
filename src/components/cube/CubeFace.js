ed8a47116bf1a1f400e9b6d8905d88e206b907dd
Aelof3 <folea3@gmail.com>
2020-10-03 18:28:40 -0400

    updated textures and each face is now broken into blocks instead of panels

--- src/components/cube/CubeFace.js
+++ src/components/cube/CubeFace.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import moles from '../../moles';
+import marios from '../../js/marios';
+import textures from '../../js/textures';
 
 class CubeFace extends Component {
     state = {
-        moles: []
+        marios: [],
+        faces: ['front','right','left','top','bottom','back'],
+        textures: textures
     }
     getRandomInt = (min,max) => {
         return Math.floor(Math.random() * max + min);
     }
     
     componentDidMount(){
-        const numbers = Array(18).fill().map((_, index) => index);
+        const numbers = Array(marios.length).fill().map((_, index) => index);
         numbers.sort(() => Math.random() - 0.5);
     
         const moleList = numbers.slice(0,9);
         
         this.setState({
-            moles: moleList.map( n => moles[n] )
+            marios: moleList.map( n => marios[n] )
         })
     }
 
@@ -24,40 +27,236 @@ class CubeFace extends Component {
         return (
             <div className={`cube__face cube__face--${this.props.face}`}>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[0]}`} />
-                    <img alt="" src={`${this.state.moles[0]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[1]}`} />
-                    <img alt="" src={`${this.state.moles[1]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[1]}`} />
+                    <img alt="" src={`${this.state.marios[1]}`} /> */}
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[2]}`} />
-                    <img alt="" src={`${this.state.moles[2]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[2]}`} />
+                    <img alt="" src={`${this.state.marios[2]}`} /> */}
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[3]}`} />
-                    <img alt="" src={`${this.state.moles[3]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[3]}`} />
+                    <img alt="" src={`${this.state.marios[3]}`} /> */}
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[4]}`} />
-                    <img alt="" src={`${this.state.moles[4]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[4]}`} />
+                    <img alt="" src={`${this.state.marios[4]}`} /> */}
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[5]}`} />
-                    <img alt="" src={`${this.state.moles[5]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[5]}`} />
+                    <img alt="" src={`${this.state.marios[5]}`} /> */}
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[6]}`} />
-                    <img alt="" src={`${this.state.moles[6]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[6]}`} />
+                    <img alt="" src={`${this.state.marios[6]}`} /> */}
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[7]}`} />
-                    <img alt="" src={`${this.state.moles[7]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[7]}`} />
+                    <img alt="" src={`${this.state.marios[7]}`} /> */}
                 </div>
                 <div className="cube__face--section">
-                    <img alt="" src={`${this.state.moles[8]}`} />
-                    <img alt="" src={`${this.state.moles[8]}`} />
+                    <div className="block">
+                        <div className={`block__face block__face--${this.state.faces[0]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[1]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[2]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[3]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[4]}`}>
+                            <img alt="" src={`${this.state.textures[24]}`} />
+                        </div>
+                        <div className={`block__face block__face--${this.state.faces[5]}`}>
+                            <img alt="" src={`${this.state.textures[35]}`} />
+                        </div>
+                    {/* <img alt="" src={`${this.state.marios[0]}`} />
+                    <img alt="" src={`${this.state.marios[0]}`} /> */}
+                    </div>
+                    {/* <img alt="" src={`${this.state.marios[8]}`} />
+                    <img alt="" src={`${this.state.marios[8]}`} /> */}
                 </div>
             </div>            
         )
---
+++ src/js/marios.js
@@ -0,0 +1,101 @@
+import mario1 from '../images/mario/mario1.png';
+import mario2 from '../images/mario/mario2.png';
+import mario3 from '../images/mario/mario3.png';
+import mario4 from '../images/mario/mario4.png';
+import mario5 from '../images/mario/mario5.png';
+import mario6 from '../images/mario/mario6.png';
+import mario7 from '../images/mario/mario7.png';
+import mario8 from '../images/mario/mario8.png';
+import mario9 from '../images/mario/mario9.png';
+import mario10 from '../images/mario/mario10.png';
+import mario11 from '../images/mario/mario11.png';
+import mario12 from '../images/mario/mario12.png';
+import mario13 from '../images/mario/mario13.png';
+import mario14 from '../images/mario/mario14.png';
+import mario15 from '../images/mario/mario15.png';
+import mario16 from '../images/mario/mario16.png';
+import mario17 from '../images/mario/mario17.png';
+import mario18 from '../images/mario/mario18.png';
+import mario19 from '../images/mario/mario19.png';
+import mario20 from '../images/mario/mario20.png';
+import mario21 from '../images/mario/mario21.png';
+import mario22 from '../images/mario/mario22.png';
+import mario23 from '../images/mario/mario23.png';
+import mario24 from '../images/mario/mario24.png';
+import mario25 from '../images/mario/mario25.png';
+import mario26 from '../images/mario/mario26.png';
+import mario27 from '../images/mario/mario27.png';
+import mario28 from '../images/mario/mario28.png';
+import mario29 from '../images/mario/mario29.png';
+import mario30 from '../images/mario/mario30.png';
+import mario31 from '../images/mario/mario31.png';
+import mario32 from '../images/mario/mario32.png';
+import mario33 from '../images/mario/mario33.png';
+import mario34 from '../images/mario/mario34.png';
+import mario35 from '../images/mario/mario35.png';
+import mario36 from '../images/mario/mario36.png';
+import mario37 from '../images/mario/mario37.png';
+import mario38 from '../images/mario/mario38.png';
+import mario39 from '../images/mario/mario39.png';
+import mario40 from '../images/mario/mario40.png';
+import mario41 from '../images/mario/mario41.png';
+import mario42 from '../images/mario/mario42.png';
+import mario43 from '../images/mario/mario43.png';
+import mario44 from '../images/mario/mario44.png';
+import mario45 from '../images/mario/mario45.png';
+import mario46 from '../images/mario/mario46.png';
+import mario47 from '../images/mario/mario47.png';
+import mario48 from '../images/mario/mario48.png';
+
+const marios = [
+    mario1,
+    mario2,
+    mario3,
+    mario4,
+    mario5,
+    mario6,
+    mario7,
+    mario8,
+    mario9,
+    mario10,
+    mario11,
+    mario12,
+    mario13,
+    mario14,
+    mario15,
+    mario16,
+    mario17,
+    mario18,
+    mario19,
+    mario20,
+    mario21,
+    mario22,
+    mario23,
+    mario24,
+    mario25,
+    mario26,
+    mario27,
+    mario28,
+    mario29,
+    mario30,
+    mario31,
+    mario32,
+    mario33,
+    mario34,
+    mario35,
+    mario36,
+    mario37,
+    mario38,
+    mario39,
+    mario40,
+    mario41,
+    mario42,
+    mario43,
+    mario44,
+    mario45,
+    mario46,
+    mario47,
+    mario48
+];
+
+export default marios;
